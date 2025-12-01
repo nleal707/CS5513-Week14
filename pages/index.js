8ec7ace0edb64b5832d5c71f0324ea5a648dfa50
Nick Leal <business.nleal@gmail.com>
2025-11-30 18:19:37 -0800

    Add transactions support

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,8 @@ export async function getStaticProps() {
     props: {
       allPostsData,
       allContactsData,
-      allProductsData
+      allProductsData,
+      allTransactionsData
     },
     revalidate: 60
   };
